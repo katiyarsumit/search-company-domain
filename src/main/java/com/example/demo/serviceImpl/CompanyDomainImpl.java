f3e806d241b1808312a9fa08bee57cf271536156
katiyarsumit <48628803+katiyarsumit@users.noreply.github.com>
2019-07-04 11:32:18 +0530

    Update CompanyDomainImpl.java

--- src/main/java/com/example/demo/serviceImpl/CompanyDomainImpl.java
+++ src/main/java/com/example/demo/serviceImpl/CompanyDomainImpl.java
@@ -21,9 +21,9 @@ public class CompanyDomainImpl implements CompanyDomain {
 	String encoding = "UTF-8";
 	//public static String filePath = "E://spring boot learning/demo/src/main/resources/static/";
 	static String userPath=System.getProperty("user.dir");
-	static String replaceString=userPath.replace('\\','/');
-	static String replaceString1=replaceString.replaceFirst("/","//");
-	public static String filePath=replaceString1+"/src/main/resources/static/";
+	//static String replaceString=userPath.replace('\\','/');
+	//static String replaceString1=replaceString.replaceFirst("/","//");
+	public static String filePath=userPath+"/src/main/resources/static/";
 	public String convertFile(MultipartFile file) {
 		String downloadUrl="";
 		try {
